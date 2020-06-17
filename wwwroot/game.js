b354ce7958ad42e6873c1e304ac9611b13cea6ec
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-06-17 09:50:40 +0200

    Added utility-fuction to generate UUID's

--- wwwroot/game.js
+++ wwwroot/game.js
@@ -307,3 +307,6 @@
   // Utility Functions
   // ...
 })(window, document);
+
+const generateUUID = () =>
+  Math.random().toString(36).substring(2) + Date.now().toString(36);
--- wwwroot/main.js
+++ wwwroot/main.js
@@ -81,3 +81,6 @@
     }
   }
 })(window, document);
+
+const generateUUID = () =>
+  Math.random().toString(36).substring(2) + Date.now().toString(36);
