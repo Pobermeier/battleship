37260c55f4d2c17d6ff6e4f01a11132b52838496
Patrick O <patrick.obermeier@outlook.com>
2021-06-25 20:31:35 +0200

    refactored getInitialGridData Util-Function

--- helpers/getInitialGridData.js
+++ helpers/getInitialGridData.js
@@ -1,16 +1,11 @@
-function getInitialGridData() {
-  return [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ];
-}
+const getInitialGridData = (width = 10, height = 10, initialCellValue = 0) => {
+  const gridArray = [];
+
+  for (let i = 0; i < height; i++) {
+    gridArray.push(new Array(width).fill(initialCellValue));
+  }
+
+  return gridArray;
+};
 
 module.exports = getInitialGridData;
--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -390,20 +390,17 @@
     `;
   }
 
+  // Utility Functions
+
   // Initial grid data
-  function getInitialGridData() {
-    return [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    ];
+  function getInitialGridData(width = 10, height = 10, initialCellValue = 0) {
+    const gridArray = [];
+
+    for (let i = 0; i < height; i++) {
+      gridArray.push(new Array(width).fill(initialCellValue));
+    }
+
+    return gridArray;
   }
 
   // Leave Game
